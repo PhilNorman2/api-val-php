22d40ca16879ccab4ca7e4d6f250d263c943c800
Norman <norman@oclc.org>
2017-09-11 14:04:13 -0400

    Initial commit of api-validation

---
+++ gitDeploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git init
+git add .
+git commit -m "Initial commit of api-validation"
+git remote add origin https://github.com/PhilNorman2/api-validation
+git push -f origin master
+
