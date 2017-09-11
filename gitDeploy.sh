6989e07dc4b0b6c314bc9755704d9a53d476fc6e
Norman <norman@oclc.org>
2017-09-11 14:19:21 -0400

    Initial commit of api-validation

--- gitDeploy.sh
+++ gitDeploy.sh
@@ -3,6 +3,9 @@
 git init
 git add .
 git commit -m "Initial commit of api-validation"
-git remote add origin https://github.com/PhilNorman2/api-validation
+#create origin first time through
+#git remote add origin https://github.com/PhilNorman2/api-validation
+#use existing origin -after first time through
+git remote set-url origin https://github.com/PhilNorman2/api-validation
 git push -f origin master
 
