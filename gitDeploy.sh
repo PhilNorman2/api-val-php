fa582d7c0df4b9cc19982892e0eaa50afdd5a690
Norman <norman@oclc.org>
2017-10-19 14:35:33 -0400

    Initial commit of api-val-php

--- gitDeploy.sh
+++ gitDeploy.sh
@@ -4,8 +4,8 @@ git init
 git add .
 git commit -m "Initial commit of api-val-php"
 #create origin first time through
-git remote add origin https://github.com/PhilNorman2/api-val-php
+#git remote add origin https://github.com/PhilNorman2/api-val-php
 #use existing origin -after first time through
-#git remote set-url origin https://github.com/PhilNorman2/api-val-php
+git remote set-url origin https://github.com/PhilNorman2/api-val-php
 git push -f origin master
 
